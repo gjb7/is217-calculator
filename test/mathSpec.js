f976f2f5495794fd2f371e3d426266309696d6a5
Grant J. Butler <butler.grant.j@gmail.com>
2014-03-08 14:47:28 -0500

    Added test for math.subtract

--- test/mathSpec.js
+++ test/mathSpec.js
@@ -21,4 +21,38 @@ describe('math', function() {
 			expect(result).to.equal(-3);
 		});
 	});
+	
+	describe('#subtract', function() {
+		it('should subtract a positive number from a positive number', function() {
+			var result = math.subtract(2, 1);
+			
+			expect(result).to.equal(1);
+			
+			var result = math.subtract(1, 2);
+			
+			expect(result).to.equal(-1);
+		});
+		
+		it('should subtract a positive number from a negative number', function() {
+			var result = math.subtract(2, -1);
+			
+			expect(result).to.equal(3);
+		});
+		
+		it('should subtract a negative number from a positive number', function() {
+			var result = math.subtract(-1, 2);
+			
+			expect(result).to.equal(-3);
+		});
+		
+		it('should subtract a negative number from a negative number', function() {
+			var result = math.subtract(-1, -2);
+			
+			expect(result).to.equal(1);
+			
+			var result = math.subtract(-2, -1);
+			
+			expect(result).to.equal(-1);
+		});
+	});
 });
