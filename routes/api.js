77cda1b86cce94441a15e8f700289526154c4afe
Grant J. Butler <butler.grant.j@gmail.com>
2014-03-09 11:46:51 -0400

    reworked API to register operations and then build routes from that.

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,57 @@
-var math = require('../lib/math');
+var math = require('../lib/math'),
+	path = require('path');
 
-function _confirmRequestHasOperands(req) {
+function API() {
+	this.operations = {};
+}
+
+API.NAME_REGEX = /^[a-zA-Z_\-]+$/;
+
+API.isValidName = function(name) {
+	return API.NAME_REGEX.test(name);
+};
+
+API.prototype.registerOperation = function(name, operation) {
+	if (!API.isValidName(name)) {
+		throw new Error("Invalid name passed for registerOperation.");
+	}
+	
+	this.operations[name] = operation;
+};
+
+API.prototype.registerRoutes = function(router) {
+	for (var name in this.operations) {
+		router.post('/api/' + name, this._performOperation.bind(this));
+	}
+};
+
+API.prototype._performOperation = function(req, res) {
+	if (!this._operationExistsForRequest(req)) {
+		return res.json(404, { error: 'Operation not found.' });
+	}
+	
+	if (!this._requestHasOperands(req)) {
+		return res.json(500, { error: 'Missing required operands.' });
+	}
+	
+	res.json({ result: this._performOperationForRequest(req) });
+};
+
+API.prototype._performOperationForRequest = function(req) {
+	var name = path.basename(req.path);
+	var operation = this.operations[name];
+	var operands = req.body.operands;
+	
+	return operation(operands[0], operands[1]);
+};
+
+API.prototype._operationExistsForRequest = function(req) {
+	var name = path.basename(req.path);
+	
+	return (this.operations[name] !== undefined);
+};
+
+API.prototype._requestHasOperands = function(req) {
 	if (!req.body.operands) {
 		return false;
 	}
@@ -14,8 +65,11 @@ function _confirmRequestHasOperands(req) {
 	}
 	
 	return true;
-}
+};
 
+exports.API = API;
+
+/*
 function _performOperation(operation, operands) {
 	return operation(operands[0], operands[1]);
 }
@@ -59,3 +113,4 @@ exports.divide = function(req, res) {
 	
 	res.json({ result: _performOperationFromRequest(math.divide, req) });
 };
+*/
