b217e5db279301498b0030c9ce54c2ba808af508
Grant J. Butler <butler.grant.j@gmail.com>
2014-03-11 14:04:02 -0400

    removed old endpoints.

--- routes/api.js
+++ routes/api.js
@@ -20,9 +20,7 @@ API.prototype.registerOperation = function(name, operation) {
 };
 
 API.prototype.registerRoutes = function(router) {
-	for (var name in this.operations) {
-		router.post('/api/' + name, this._performOperation.bind(this));
-	}
+	router.post('/api/:name', this._performOperation.bind(this));
 };
 
 API.prototype._performOperation = function(req, res) {
@@ -39,7 +37,7 @@ API.prototype._performOperation = function(req, res) {
 
 API.prototype._performOperationForRequest = function(req) {
 	var name = path.basename(req.path);
-	var operation = this.operations[name];
+	var operation = this.operations[req.paga];
 	var operands = req.body.operands;
 	
 	return operation(operands[0], operands[1]);
@@ -68,49 +66,3 @@ API.prototype._requestHasOperands = function(req) {
 };
 
 exports.API = API;
-
-/*
-function _performOperation(operation, operands) {
-	return operation(operands[0], operands[1]);
-}
-
-exports.add = function(req, res) {
-	if (!_confirmRequestHasOperands(req)) {
-		res.json({ error: 'Missing required operands.' });
-		
-		return;
-	}
-	
-	res.json({ result: _performOperationFromRequest(math.add, req) });
-};
-
-exports.subtract = function(req, res) {
-	if (!_confirmRequestHasOperands(req)) {
-		res.json({ error: 'Missing required operands.' });
-		
-		return;
-	}
-	
-	res.json({ result: _performOperationFromRequest(math.subtract, req) });
-};
-
-exports.multiply = function(req, res) {
-	if (!_confirmRequestHasOperands(req)) {
-		res.json({ error: 'Missing required operands.' });
-		
-		return;
-	}
-	
-	res.json({ result: _performOperationFromRequest(math.multiply, req) });
-};
-
-exports.divide = function(req, res) {
-	if (!_confirmRequestHasOperands(req)) {
-		res.json({ error: 'Missing required operands.' });
-		
-		return;
-	}
-	
-	res.json({ result: _performOperationFromRequest(math.divide, req) });
-};
-*/
